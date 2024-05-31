77619682a1ef9ef1fd04b98f5a1890d5627dc88f
Carlos Augusto <carlos.aug.developer@gmail.com>
2024-05-31 14:37:27 -0300

    chore

---
+++ src/main/java/br/com/sari_backend/SariBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.sari_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SariBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SariBackendApplication.class, args);
+	}
+
+}

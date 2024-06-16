4729c57875f14e3245000940c6fb869b1d9cfab3
Carlos Augusto <carlos.brandao@etipi.pi.gov.br>
2024-06-16 08:19:00 -0300

    feat: created generic mapper reflection overriding toObject function
    - Reflection does not initialize an object of a Class, so, default values ​​started in the constructor are not called. I needed to do that "manually" and verify when or not substitute the default value initialized in the constructor through the reflection

--- src/main/java/br/com/sari_backend/mappers/GenericMapper.java
+++ src/main/java/br/com/sari_backend/mappers/GenericMapper.java
@@ -1,5 +1,6 @@
 package br.com.sari_backend.mappers;
 
+import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Objects;
 
@@ -26,6 +27,45 @@ public class GenericMapper implements IGenericMapper {
     }
 
     return mapper.convertValue(obj, clazz);
+  }
+
+  public <S, D> D toObject(S source, Class<D> destinationClass, boolean isNewRecord) {
+    D destination;
+
+    try {
+      destination = destinationClass.getDeclaredConstructor().newInstance();
+
+      Field[] sourceFields = source.getClass().getDeclaredFields();
+
+      for (Field sourceField : sourceFields) {
+        sourceField.setAccessible(true);
+
+        Object value = sourceField.get(source);
+
+        try {
+          Field destinationField = destinationClass.getDeclaredField(sourceField.getName());
+          destinationField.setAccessible(true);
+
+          if (value != null) {
+
+            // NOTE: This does not need anymore because the conversion between String to the
+            // type Enum, is made automaticly whne i add role with type RoleEnum, and not
+            // String in CreateUserDTO
+            // if (destinationField.getType().isEnum()) {
+            // value = Enum.valueOf((Class<Enum>) destinationField.getType(),
+            // value.toString());
+            // }
+            destinationField.set(destination, value);
+          }
+        } catch (NoSuchFieldException e) {
+          System.out.println(e);
+        }
+      }
+    } catch (Exception e) {
+      throw new RuntimeException("Error mapping objects - " + e, e);
+    }
+
+    return destination;
   }
 
   // TODO: implement toList mapper

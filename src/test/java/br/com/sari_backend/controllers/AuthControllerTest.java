e518f286973e243e0f9e0cb3fb727338b1f3a864
Carlos Augusto <carlos.brandao@etipi.pi.gov.br>
2024-06-23 21:38:37 -0300

    chore: configured test container with postgresql

--- src/test/java/br/com/sari_backend/controllers/AuthControllerTest.java
+++ src/test/java/br/com/sari_backend/controllers/AuthControllerTest.java
@@ -1,11 +1,5 @@
 package br.com.sari_backend.controllers;
 
-import static org.mockito.ArgumentMatchers.any;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -13,22 +7,18 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import br.com.sari_backend.controllers.AuthController;
-import br.com.sari_backend.dtos.user.AuthDTO;
 import br.com.sari_backend.services.IAuthService;
-
-import static org.mockito.BDDMockito.*;
 
 @WebMvcTest(AuthController.class)
 public class AuthControllerTest {
-    @Autowired
-    private MockMvc mockMvc;
+    // @Autowired
+    // private MockMvc mockMvc;
 
-    @Autowired
-    private ObjectMapper objectMapper;
+    // @Autowired
+    // private ObjectMapper objectMapper;
 
-    @MockBean
-    private IAuthService authService;
+    // @MockBean
+    // private IAuthService authService;
 
     // @Test
     // public void whenLoginWithValidCredentials_thenReturn200AndToken() throws

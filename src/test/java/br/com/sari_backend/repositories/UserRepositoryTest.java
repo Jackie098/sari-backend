f9446b42e45755e8ba4ba2fd521bfb2d6907ed47
Carlos Augusto <carlos.brandao@etipi.pi.gov.br>
2024-06-23 21:38:51 -0300

    feat: created test for create user repository

--- src/test/java/br/com/sari_backend/repositories/UserRepositoryTest.java
+++ src/test/java/br/com/sari_backend/repositories/UserRepositoryTest.java
@@ -5,25 +5,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.time.Instant;
 import java.util.Optional;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ActiveProfiles;
 
+import br.com.sari_backend.SariBackendApplicationTests;
 import br.com.sari_backend.dtos.user.CreateUserDTO;
 import br.com.sari_backend.models.User;
-import jakarta.persistence.EntityManager;
 
-@DataJpaTest
-@ActiveProfiles("test")
-public class UserRepositoryTest {
+// @DataJpaTest
+// @ActiveProfiles("test")
+public class UserRepositoryTest extends SariBackendApplicationTests {
 
   @Autowired
   UserRepository userRepository;
 
-  @Autowired
-  EntityManager entityManager;
+  // @BeforeEach
+  // void setUp() {
+  // System.out.println("Running setUp beforeEach test");
+  // }
 
   @Test
   @DisplayName("Should get User successfully from DB")
@@ -47,14 +48,18 @@ public class UserRepositoryTest {
     User newUser = new User();
 
     newUser.setName(data.getName());
-    // newUser.setPassword(data.getPassword());
-    // newUser.setPhone(data.getPhone());
-    // newUser.setRole(data.getRole());
-    // newUser.setCreatedAt(Instant.now());
-    // newUser.setUpdatedAt(Instant.now());
+    newUser.setEmail(data.getEmail());
+    newUser.setPassword(data.getPassword());
+    newUser.setPhone(data.getPhone());
 
-    this.entityManager.persist(newUser);
-    // this.entityManager.flush();
+    if (data.getRole() != null) {
+      newUser.setRole(data.getRole());
+    }
+
+    newUser.setCreatedAt(Instant.now());
+    newUser.setUpdatedAt(Instant.now());
+
+    this.userRepository.save(newUser);
 
     return newUser;
   }
